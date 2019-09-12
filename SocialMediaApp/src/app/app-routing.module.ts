59461a8396e5f25021f09ed4521d90d19624c6bb
unknown <qwbnot@gmail.com>
2019-09-12 12:20:31 -0400

    added login, registration and password recovery pages.

--- SocialMediaApp/src/app/app-routing.module.ts
+++ SocialMediaApp/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { PassRecoveryComponent } from './pass-recovery/pass-recovery.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent},
+  { path: 'pass-recovery', component: PassRecoveryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
