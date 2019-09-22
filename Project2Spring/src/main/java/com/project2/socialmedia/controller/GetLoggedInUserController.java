1c25356a5bec2268d18f3c0ef57fec9d6bf39afc
Nick Helgeland <nickhelgeland94@gmail.com>
2019-09-21 20:24:59 -0400

    Started adding functionality for retreiving images

--- Project2Spring/src/main/java/com/project2/socialmedia/controller/GetLoggedInUserController.java
+++ Project2Spring/src/main/java/com/project2/socialmedia/controller/GetLoggedInUserController.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project2.socialmedia.dao.UserDAO;
@@ -17,10 +19,9 @@ public class GetLoggedInUserController
 	UserDAO userRepo;
 	
 	@CrossOrigin
-	@GetMapping(value="/getLoggedInUser")
-	public Users getLoggedInUser(HttpSession httpSession)
+	@PostMapping(value="/getLoggedInUser")
+	public Users getLoggedInUser(@RequestBody String username)
 	{
-		String username = httpSession.getAttribute("username").toString();
 		Users user = userRepo.selectOne(username);
 		
 		return user;
---
+++ Project2Spring/src/main/java/com/project2/socialmedia/controller/GetProfileImageController.java
@@ -0,0 +1,5 @@
+package com.project2.socialmedia.controller;
+
+public class GetProfileImageController {
+
+}
