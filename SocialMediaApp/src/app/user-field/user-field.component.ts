4f336aa2c53095e3b9d2e3fdb317cd4347f0922e
Nick Helgeland <nickhelgeland94@gmail.com>
2019-09-22 11:01:38 -0400

    Completed s3 integration on the profile page.

--- SocialMediaApp/src/app/user-field/user-field.component.ts
+++ SocialMediaApp/src/app/user-field/user-field.component.ts
@@ -8,14 +8,17 @@ import { SessionService } from '../session.service';
   templateUrl: './user-field.component.html',
   styleUrls: ['./user-field.component.css']
 })
-export class UserFieldComponent implements OnInit {
+export class UserFieldComponent implements OnInit { 
 
   baseUrl: string = "http://localhost:9005/Project2Spring/api/"
+
+  src: string = "../../assets/user.png"
 
   tData: string = '';
   likeCounter: number = 0;
 
-  async uploadFile(event) {
+  async uploadFile(event)
+  {
     let file = event.target.files[0]
     let urlResponse = await fetch(this.baseUrl + 's3/' + file.name, {
       method: 'PUT'
@@ -27,16 +30,33 @@ export class UserFieldComponent implements OnInit {
       body: file
     })
     this.updateProfilePicture(file.name)
+    this.getFile()
   }
 
-  updateProfilePicture(filename: string) {
+  updateProfilePicture(filename: string)
+  {
     let image = {
-      name: filename,
-      username: this._session.getUsername()
+      name : filename,
+      username : this._session.getUsername()
     }
 
     this._http.post(this.baseUrl + 'updateImage', image).subscribe()
     alert("Image has been uploaded")
+  }
+
+  async getFile()
+  {
+    let getImageRespone = await fetch(this.baseUrl + 'getProfilePic/' + this._session.username, {
+        method: 'GET'
+    })
+    let imageName = await getImageRespone.text()
+
+    let urlResponse = await fetch(this.baseUrl + 's3/' + imageName, {
+    method: 'GET'
+    });
+    let signedUrl = await urlResponse.text();
+
+    this.src = signedUrl
   }
 
   likeCount() {
@@ -46,12 +66,10 @@ export class UserFieldComponent implements OnInit {
   inputData() {
     this.tData = this.tData;
   }
+  
+  constructor(private _http: HttpClient, private _session: SessionService) { this.getFile() }
 
-  togglePage() {
-    this._toggle.toggleLogOut();
-  }
-
-  constructor(private _http: HttpClient, private _session: SessionService, private _toggle: AppComponent) { }  
-
-  ngOnInit() { }
+  
+  
+  ngOnInit() { }  
 }
